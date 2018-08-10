95e6f97e737a2f85f52cf9238d639f6939e21df4
Dveenadhari <41769824+Dveenadhari@users.noreply.github.com>
2018-08-10 12:30:08 +0530

    Update veena16.py

--- veena16.py
+++ veena16.py
@@ -1,8 +1,3 @@
-def power(base,exp):
-   if(exp==1):
-       return(base)
-   if(exp!=1):
-      return(base*power(base,exp-1))
-base=int(input("enter base:"))
-exp=int(input("enter exponential value:"))
-print("result:",power(base,exp))
+x=raw-input()
+a,b=x.spilt()
+print (a**b)
